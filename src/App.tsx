fb1903ea609619e8d8b890df2a31d1040b81cb8c
quyet <quyet@flynotes.co.uk>
2021-03-11 14:11:15 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
+import { ThemeProvider } from "@material-ui/core";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
-import { theme } from "./theme/muiTheme";
-import { Box, ThemeProvider } from "@material-ui/core";
 import Layout from "./components/Layout/Layout";
-import New from "./components/New";
+import { theme } from "./theme/muiTheme";
 
 function App() {
-	const arr = [10, 9, 8, 7];
-	const bubbleSort = (arr: number[]) => {
-		for (let i = 0; i < arr.length - 1; i++) {
-			for (let j = 0; j < arr.length - 1 - i; j++) {
-
-				if (arr[j] > arr[j + 1]) {
-					const temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-				console.log(j, j+1);
-				
-				console.log(arr);
-			}
-		}
-		console.log(arr.length);
-
-		return arr;
-	};
-
 	return (
 		<ThemeProvider theme={theme}>
 			<Layout>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,8 @@
-import { Box, Button, Grid, Typography } from "@material-ui/core";
-import { Add } from "@material-ui/icons";
-import React, { useEffect } from "react";
-import { customerController } from "../controllers";
+import { Box, Grid, Typography } from "@material-ui/core";
+import React from "react";
 import ProductTable2 from "./ProductTable2";
 
 export default function Home() {
-	useEffect(() => {
-		customerController.get({ page: 2, pageSize: 10 }).then((res) => console.log(res));
-	}, []);
 	return (
 		<Grid container>
 			<Grid item container direction="row" justify="space-between">
